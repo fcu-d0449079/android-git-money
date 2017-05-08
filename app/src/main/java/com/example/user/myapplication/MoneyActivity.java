106a24d4331da5d19ae368c600fad8b18f0dc8e6
fcu-d0449079 <ink_zhuang@yahoo.com.tw>
2017-05-09 01:16:08 +0800

    0115

--- app/src/main/java/com/example/user/myapplication/MoneyActivity.java
+++ app/src/main/java/com/example/user/myapplication/MoneyActivity.java
@@ -1,30 +1,38 @@
 package com.example.user.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 
 public class MoneyActivity extends AppCompatActivity {
-
-
-
-
-
+    Button btn_input;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_money);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
+        btn_input = (Button)findViewById(R.id.btn_input);
+        btn_input.setOnClickListener(userinputmoney);
     }
+    private View.OnClickListener userinputmoney = new View.OnClickListener(){
+
+        public void onClick(View v){
+            Intent intent = new Intent();
+            intent.setClass(MoneyActivity.this,inputmoney.class);
+            startActivity(intent);
+            Toast.makeText(MoneyActivity.this,"使用記帳功能",Toast.LENGTH_SHORT).show();
+
+        }
+    };
 
 }
---
+++ app/src/main/java/com/example/user/myapplication/inputmoney.java
@@ -0,0 +1,21 @@
+package com.example.user.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by Zhuang on 2017/5/9.
+ */
+
+public class inputmoney extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_inputmoney);
+
+    }
+
+
+}
