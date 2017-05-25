5e14bb4f1bd9d4dd4332b45ecfbd80e0809800ed
fcu-d0449079 <ink_zhuang@yahoo.com.tw>
2017-05-25 15:49:10 +0800

    calculator 1

--- app/src/main/java/com/example/user/myapplication/input_money.java
+++ app/src/main/java/com/example/user/myapplication/input_money.java
@@ -6,9 +6,12 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class input_money extends AppCompatActivity {
-
+    Button bt0,bt1,bt2,bt3,bt4,bt5,bt6,bt7,bt8,bt9,bta,btb,btc,btAC;
+    TextView total,show;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +19,25 @@ public class input_money extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        bt0=(Button)findViewById(R.id.bt0);
+        bt1=(Button)findViewById(R.id.bt1);
+        bt2=(Button)findViewById(R.id.bt2);
+        bt3=(Button)findViewById(R.id.bt3);
+        bt4=(Button)findViewById(R.id.bt4);
+        bt5=(Button)findViewById(R.id.bt5);
+        bt6=(Button)findViewById(R.id.bt6);
+        bt7=(Button)findViewById(R.id.bt7);
+        bt8=(Button)findViewById(R.id.bt8);
+        bt9=(Button)findViewById(R.id.bt9);
+        bta=(Button)findViewById(R.id.bta);
+        btb=(Button)findViewById(R.id.btc);
+        btc=(Button)findViewById(R.id.btc);
+        btAC=(Button)findViewById(R.id.btAC);
+
+        show=(TextView)findViewById(R.id.show);
+        total=(TextView)findViewById(R.id.total);
+
     }
+
 
 }
