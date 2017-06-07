3f79f9485761a5ecfca23f6d13517ef56ec15c68
fcu-d0449079 <ink_zhuang@yahoo.com.tw>
2017-06-07 23:29:18 +0800

    Calculator finish

--- app/src/main/java/com/example/user/myapplication/input_money.java
+++ app/src/main/java/com/example/user/myapplication/input_money.java
@@ -10,8 +10,12 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class input_money extends AppCompatActivity {
-    Button bt0,bt1,bt2,bt3,bt4,bt5,bt6,bt7,bt8,bt9,bta,btb,btc,btd,btAC;
-    TextView total,show;
+    int now, last = 0;
+    String abc;
+    Button bt0, bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9, btplus, btsub, btdiv, btmul, btAC,btequ;
+    TextView total;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,26 +23,121 @@ public class input_money extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        bt0=(Button)findViewById(R.id.bt0);
-        bt1=(Button)findViewById(R.id.bt1);
-        bt2=(Button)findViewById(R.id.bt2);
-        bt3=(Button)findViewById(R.id.bt3);
-        bt4=(Button)findViewById(R.id.bt4);
-        bt5=(Button)findViewById(R.id.bt5);
-        bt6=(Button)findViewById(R.id.bt6);
-        bt7=(Button)findViewById(R.id.bt7);
-        bt8=(Button)findViewById(R.id.bt8);
-        bt9=(Button)findViewById(R.id.bt9);
-        bta=(Button)findViewById(R.id.bta);
-        btb=(Button)findViewById(R.id.btb);
-        btc=(Button)findViewById(R.id.btc);
-        btd=(Button)findViewById(R.id.btd);
-        btAC=(Button)findViewById(R.id.btAC);
-
-        show=(TextView)findViewById(R.id.show);
-        total=(TextView)findViewById(R.id.total);
-
+       find();
     }
 
+    private Button.OnClickListener click = new Button.OnClickListener()
+
+    {
+        @Override
+        public void onClick (View v){
+        {
+            if (v.getId() == R.id.bt0) {
+                now =  now*10 + 0;
+            } else if (v.getId() == R.id.bt1) {
+                now = now*10 + 1;
+            } else if (v.getId() == R.id.bt2) {
+                now = now*10 + 2;
+            } else if (v.getId() == R.id.bt3) {
+                now = now*10 + 3;
+            } else if (v.getId() == R.id.bt4) {
+                now = now*10 + 4;
+            } else if (v.getId() == R.id.bt5) {
+                now = now*10 + 5;
+            } else if (v.getId() == R.id.bt6) {
+                now = now*10 + 6;
+            } else if (v.getId() == R.id.bt7) {
+                now = now*10 + 7;
+            } else if (v.getId() == R.id.bt8) {
+                now = now*10 + 8;
+            } else if (v.getId() == R.id.bt9) {
+                now = now*10 + 9;
+            } else if (v.getId() == R.id.btAC) {
+                now = 0;
+            }
+            total.setText(String.valueOf(now));
+        }
+        if (v.getId() == R.id.btplus) {
+            last = now;
+            now = 0;
+            abc = btplus.getText().toString();
+            total.setText(String.valueOf(abc));
+        } else if (v.getId() == R.id.btsub) {
+            last = now;
+            now = 0;
+            abc = btsub.getText().toString();
+            total.setText(String.valueOf(abc));
+        } else if (v.getId() == R.id.btmul) {
+            last = now;
+            now = 0;
+            abc = btmul.getText().toString();
+            total.setText(String.valueOf(abc));
+        } else if (v.getId() == R.id.btdiv) {
+            last = now;
+            now = 0;
+            abc = btdiv.getText().toString();
+            total.setText(String.valueOf(abc));
+        }
+
+        if (v.getId() == R.id.btequ) {
+            if (abc.equals("+")) {
+                now = last + now;
+                total.setText(String.valueOf(now));
+                last = 0;
+            } else if (abc.equals("-")) {
+                now = last - now;
+                total.setText(String.valueOf(now));
+                last = 0;
+            } else if (abc.equals("*")) {
+                now = last * now;
+                total.setText(String.valueOf(now));
+                last = 0;
+            } else if (abc.equals("/")) {
+                now = last / now;
+                total.setText(String.valueOf(now));
+                last = 0;
+            }
+            else if (abc.equals("="))
+                now = last;
+                total.setText(String.valueOf(now));
+                last = 0;
+        }
+    }
+    };
+    private void find(){
+        bt0 = (Button) findViewById(R.id.bt0);
+        bt1 = (Button) findViewById(R.id.bt1);
+        bt2 = (Button) findViewById(R.id.bt2);
+        bt3 = (Button) findViewById(R.id.bt3);
+        bt4 = (Button) findViewById(R.id.bt4);
+        bt5 = (Button) findViewById(R.id.bt5);
+        bt6 = (Button) findViewById(R.id.bt6);
+        bt7 = (Button) findViewById(R.id.bt7);
+        bt8 = (Button) findViewById(R.id.bt8);
+        bt9 = (Button) findViewById(R.id.bt9);
+        btplus = (Button) findViewById(R.id.btplus);
+        btsub = (Button) findViewById(R.id.btsub);
+        btmul = (Button) findViewById(R.id.btmul);
+        btdiv = (Button) findViewById(R.id.btdiv);
+        btAC = (Button) findViewById(R.id.btAC);
+        btequ = (Button) findViewById(R.id.btequ);
+        total = (TextView) findViewById(R.id.total);
+
+        bt1.setOnClickListener(click);
+        bt2.setOnClickListener(click);
+        bt3.setOnClickListener(click);
+        bt4.setOnClickListener(click);
+        bt5.setOnClickListener(click);
+        bt6.setOnClickListener(click);
+        bt7.setOnClickListener(click);
+        bt8.setOnClickListener(click);
+        bt9.setOnClickListener(click);
+        btAC.setOnClickListener(click);
+        btplus.setOnClickListener(click);
+        btdiv.setOnClickListener(click);
+        btsub.setOnClickListener(click);
+        btmul.setOnClickListener(click);
+        btequ.setOnClickListener(click);
+    }
 
 }
