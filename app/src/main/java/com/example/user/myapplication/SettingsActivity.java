8201d97ea52eaf42f3d40d9efac481ccc8de8d37
Dai <d0598299@fcu.edu.tw>
2017-06-13 22:37:27 +0800

    新增 設定內容

--- app/src/main/java/com/example/user/myapplication/SettingsActivity.java
+++ app/src/main/java/com/example/user/myapplication/SettingsActivity.java
@@ -6,8 +6,15 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    boolean logon = false;
+    public static final int FUNC_LOGIN=1;
+    String func[] = {"製作團隊","聯絡我們","贊助我們"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +22,12 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ListView list = (ListView)findViewById(R.id.list);
+        ArrayAdapter adapter= new ArrayAdapter(this,android.R.layout.simple_list_item_1,func);
+        list.setAdapter(adapter);
 
-    }
 
+
+
+}
 }
