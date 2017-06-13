c9b2d38dca2c105b80653aaa7b092f2c4f0fa397
Dai <d0598299@fcu.edu.tw>
2017-06-14 00:35:07 +0800

    設定內容修正

--- app/src/main/java/com/example/user/myapplication/SettingsActivity.java
+++ app/src/main/java/com/example/user/myapplication/SettingsActivity.java
@@ -1,33 +1,59 @@
 package com.example.user.myapplication;
 
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class SettingsActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
-    boolean logon = false;
-    public static final int FUNC_LOGIN=1;
-    String func[] = {"製作團隊","聯絡我們","贊助我們"};
+public class SettingsActivity extends ListActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        ListView list = (ListView)findViewById(R.id.list);
-        ArrayAdapter adapter= new ArrayAdapter(this,android.R.layout.simple_list_item_1,func);
+        //setContentView(R.layout.activity_settings);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        ListView list = getListView();
+        String[] menuItem = {"製作團隊","聯絡我們","贊助我們"};
+        ArrayAdapter<String> adapter= new ArrayAdapter(this,android.R.layout.simple_list_item_1,menuItem);
         list.setAdapter(adapter);
 
 
-
-
 }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        switch (position){
+            case 0:
+                AlertDialog.Builder ad = new AlertDialog.Builder(this);
+                ad.setTitle("關於本程式");
+                ad.setMessage("作者：戴毓辰");
+
+                DialogInterface.OnClickListener listener =
+                        new DialogInterface.OnClickListener(){
+                            public void onClick(DialogInterface di,int i){
+
+                            }
+                        };
+                ad.setPositiveButton("確定",listener);
+                ad.show();
+                break;
+
+        }
+
+
+    }
+
+
+
 }
